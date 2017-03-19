3e12c033fae2ba30db712b5ebb0c4753d1440f33
Oscar Franco <ospfranco@gmail.com>
2017-03-19 07:49:48 +0100

    [ENHANCEMENT] Improve code quality a bit

--- index.js
+++ index.js
@@ -4,24 +4,25 @@
 
 import { NativeModules } from 'react-native';
 
-const cheerio = require('cheerio-without-node-native')
-const Autolinker = require('autolinker')
+const cheerio = require('cheerio-without-node-native');
+const Autolinker = require('autolinker');
 
 export default class LinkPreview {
   static getPreview(text) {
     return new Promise((resolve, reject) => {
       if(!text) {
-        reject({error: 'LinkPreview did not receive either a url or text'})
+        reject({error: 'React-Native-Link-Preview did not receive either a url or text'});
       }
 
-      let detectedUrl = null
-      const linkedText = Autolinker.link(text, {
+      let detectedUrl = null;
+
+      Autolinker.link(text, {
         replaceFn: match => {
           switch( match.getType() ) {
           case 'url' :
             if(!detectedUrl)
-              detectedUrl = match.getUrl()
-            return true
+              detectedUrl = match.getUrl();
+            return true;
           default:
             break
           }
@@ -34,16 +35,16 @@ export default class LinkPreview {
         .then(text => {
           resolve(this._parseResponse(text, detectedUrl))
         })
-        .catch(err => reject(err))
+        .catch(error => reject({error}));
       } else {
-        reject({error: 'Preview link did not find a link in the text'})
+        reject({error: 'Preview link did not find a link in the text'});
       }
     })
   }
 
 
   static _parseResponse(body, url) {
-    const doc = cheerio.load(body)
+    const doc = cheerio.load(body);
 
     return {
       url,
@@ -52,58 +53,53 @@ export default class LinkPreview {
       mediaType: this._getMediaType(doc) || 'website',
       images: this._getImages(doc, url),
       videos: this._getVideos(doc),
-    }
+    };
   }
 
-  static _getTitle(doc){
-    let title = doc('title').text()
+  static _getTitle(doc) {
+    let title = doc('title').text();
 
-    if(title === undefined || !title){
-      title = doc('meta[property=\'og:title\']').attr('content')
+    if(!title){
+      title = doc('meta[property=\'og:title\']').attr('content');
     }
 
-    return title
+    return title;
   }
 
   static _getDescription(doc){
-    let description = doc('meta[name=description]').attr('content')
+    let description = doc('meta[name=description]').attr('content');
 
     if(description === undefined) {
-      description = doc('meta[name=Description]').attr('content')
+      description = doc('meta[name=Description]').attr('content');
+    }
 
-      if(description === undefined) {
-        description = doc('meta[property=\'og:description\']').attr('content')
-      }
+    if(description === undefined) {
+      description = doc('meta[property=\'og:description\']').attr('content');
     }
 
     return description
   }
 
   static _getMediaType(doc) {
-    let node = doc('meta[name=medium]'),
-      content
+    let node = doc('meta[name=medium]');
 
     if(node.length) {
-      content = node.attr('content')
-      return content == 'image' ? 'photo' : content
+      const content = node.attr('content');
+      return content == 'image' ? 'photo' : content;
     } else {
-      return doc('meta[property=\'og:type\']').attr('content')
+      return doc('meta[property=\'og:type\']').attr('content');
     }
   }
 
   static _getImages(doc, rootUrl) {
-    let images = [], nodes, src,
-      width, height,
-      dic
+    let images = [], nodes, src;
 
     nodes = doc('meta[property=\'og:image\']')
 
     if(nodes.length > 0) {
-      nodes.each((index, node) => {
-        src = node.attribs['content']
-        images.push(src)
-      })
+      nodes.each((index, node) => { images.push(node.attribs['content']); })
     }
+
     return images
   }
 
@@ -147,9 +143,9 @@ export default class LinkPreview {
 
   static _parseMediaResponse(res, contentType, url) {
     if(contentType.indexOf('image/') === 0) {
-      return createResponseData(url, false, '', '', contentType, 'photo', [url])
+      return createResponseData(url, false, '', '', contentType, 'photo', [url]);
     } else {
-      return createResponseData(url, false, '', '', contentType)
+      return createResponseData(url, false, '', '', contentType);
     }
   }
 
