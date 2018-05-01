1a47ca4936e586887c67a76feb05bd06118e0e63
Steven Pal <steven.pal@gmail.com>
2018-05-01 15:20:06 -0700

    Added different response objects depending on content type returned in HTTP response headers. Also fixed a bug with redirected URLs.

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,5 +1,5 @@
-import LinkPreview from '../index.js';
-import expect from 'expect.js';
+const LinkPreview = require('../index.js');
+const expect = require('expect.js');
 
 global.fetch = require('node-fetch');
 
@@ -16,10 +16,12 @@ describe('link preview', () => {
     expect(linkInfo.url).to.be.equal('https://www.youtube.com/watch?v=wuClZjOdT30');
     expect(linkInfo.title).to.be.equal('Geography Now! Germany');
     expect(linkInfo.description).to.be.ok();
-    expect(linkInfo.mediaType).to.be.equal('video');
+    expect(linkInfo.mediaType).to.be.equal('video.other');
     expect(linkInfo.images.length).to.be.equal(1);
     expect(linkInfo.images[0]).to.be.equal('https://i.ytimg.com/vi/wuClZjOdT30/maxresdefault.jpg');
     expect(linkInfo.videos.length).to.be.equal(0);
+    expect(linkInfo.favicons[0]).to.be.equal('https://www.youtube.com/yts/img/favicon_32-vflOogEID.png');
+    expect(linkInfo.contentType).to.be.equal('text/html; charset=utf-8');
   });
 
   it('should extract link info from just text with a URL', async () => {
@@ -34,10 +36,68 @@ describe('link preview', () => {
     expect(linkInfo.url).to.be.equal('https://www.youtube.com/watch?v=wuClZjOdT30');
     expect(linkInfo.title).to.be.equal('Geography Now! Germany');
     expect(linkInfo.description).to.be.ok();
-    expect(linkInfo.mediaType).to.be.equal('video');
+    expect(linkInfo.mediaType).to.be.equal('video.other');
     expect(linkInfo.images.length).to.be.equal(1);
     expect(linkInfo.images[0]).to.be.equal('https://i.ytimg.com/vi/wuClZjOdT30/maxresdefault.jpg');
     expect(linkInfo.videos.length).to.be.equal(0);
+    expect(linkInfo.favicons[0]).to.be.equal('https://www.youtube.com/yts/img/favicon_32-vflOogEID.png');
+    expect(linkInfo.contentType).to.be.equal('text/html; charset=utf-8');
+  });
+
+  it('should handle audio urls', async () => {
+    const linkInfo = await LinkPreview.getPreview('https://ondemand.npr.org/anon.npr-mp3/npr/atc/2007/12/20071231_atc_13.mp3');
+
+    // { url: 'https://ondemand.npr.org/anon.npr-mp3/npr/atc/2007/12/20071231_atc_13.mp3',
+    //   mediaType: 'audio',
+    //   contentType: 'audio/mpeg',
+    //   favicons: [ 'https://ondemand.npr.org/favicon.ico' ] }
+
+    expect(linkInfo.url).to.be.equal('https://ondemand.npr.org/anon.npr-mp3/npr/atc/2007/12/20071231_atc_13.mp3');
+    expect(linkInfo.mediaType).to.be.equal('audio');
+    expect(linkInfo.contentType).to.be.equal('audio/mpeg');
+    expect(linkInfo.favicons[0]).to.be.ok();
+  });
+
+  it('should handle video urls', async () => {
+    const linkInfo = await LinkPreview.getPreview('https://www.w3schools.com/html/mov_bbb.mp4');
+
+    // { url: 'https://www.w3schools.com/html/mov_bbb.mp4',
+    //   mediaType: 'video',
+    //   contentType: 'video/mp4',
+    //   favicons: [ 'https://www.w3schools.com/favicon.ico' ] }
+
+    expect(linkInfo.url).to.be.equal('https://www.w3schools.com/html/mov_bbb.mp4');
+    expect(linkInfo.mediaType).to.be.equal('video');
+    expect(linkInfo.contentType).to.be.equal('video/mp4');
+    expect(linkInfo.favicons[0]).to.be.ok();
+  });
+
+  it('should handle image urls', async () => {
+    const linkInfo = await LinkPreview.getPreview('https://media.npr.org/assets/img/2018/04/27/gettyimages-656523922nunes-4bb9a194ab2986834622983bb2f8fe57728a9e5f-s1100-c15.jpg');
+
+    // { url: 'https://media.npr.org/assets/img/2018/04/27/gettyimages-656523922nunes-4bb9a194ab2986834622983bb2f8fe57728a9e5f-s1100-c15.jpg',
+    //   mediaType: 'image',
+    //   contentType: 'image/jpeg',
+    //   favicons: [ 'https://media.npr.org/favicon.ico' ] }
+
+    expect(linkInfo.url).to.be.equal('https://media.npr.org/assets/img/2018/04/27/gettyimages-656523922nunes-4bb9a194ab2986834622983bb2f8fe57728a9e5f-s1100-c15.jpg');
+    expect(linkInfo.mediaType).to.be.equal('image');
+    expect(linkInfo.contentType).to.be.equal('image/jpeg');
+    expect(linkInfo.favicons[0]).to.be.ok();
+  });
+
+  it('should handle application urls', async () => {
+    const linkInfo = await LinkPreview.getPreview('https://assets.curtmfg.com/masterlibrary/56282/installsheet/CME_56282_INS.pdf');
+
+    // { url: 'https://assets.curtmfg.com/masterlibrary/56282/installsheet/CME_56282_INS.pdf',
+    //  mediaType: 'application',
+    //  contentType: 'application/pdf',
+    //  favicons: [ 'https://assets.curtmfg.com/favicon.ico' ] }
+
+    expect(linkInfo.url).to.be.equal('https://assets.curtmfg.com/masterlibrary/56282/installsheet/CME_56282_INS.pdf');
+    expect(linkInfo.mediaType).to.be.equal('application');
+    expect(linkInfo.contentType).to.be.equal('application/pdf');
+    expect(linkInfo.favicons[0]).to.be.ok();
   });
 
   it('no link in text should fail gracefully', async () => {
