72872450281b7467d3e99d345b4dac14abcf4bdd
Megan Taylor <mtaylor@megantaylor.org>
2020-10-29 18:45:57 -0400

    fix linting

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,28 +1,28 @@
 interface ILinkPreviewOptions {
-  headers?: Record<string, string>;
-  imagesPropertyType?: string;
-  proxyUrl?: string;
+    headers?: Record<string, string>;
+    imagesPropertyType?: string;
+    proxyUrl?: string;
 }
 export declare function getLinkPreview(text: string, options?: ILinkPreviewOptions): Promise<{
-  url: string;
-  mediaType: string;
-  contentType: string;
-  favicons: any[];
+    url: string;
+    mediaType: string;
+    contentType: string;
+    favicons: any[];
 } | {
-  url: string;
-  title: any;
-  siteName: any;
-  description: any;
-  mediaType: any;
-  contentType: string | undefined;
-  images: string[];
-  videos: {
-    url: any;
-    secureUrl: any;
-    type: any;
-    width: any;
-    height: any;
-  }[];
-  favicons: any[];
+    url: string;
+    title: any;
+    siteName: any;
+    description: any;
+    mediaType: any;
+    contentType: string | undefined;
+    images: string[];
+    videos: {
+        url: any;
+        secureUrl: any;
+        type: any;
+        width: any;
+        height: any;
+    }[];
+    favicons: any[];
 }>;
 export {};
