42b035d261ca66141824cc7c6af609f8621b8b2f
wafuwafu13 <mariobaske@i.softbank.jp>
2021-01-28 19:21:25 +0900

    fix node and src type in getImages function

--- index.ts
+++ index.ts
@@ -54,19 +54,21 @@ function getMediaType(doc: cheerio.Root) {
 
 function getImages(doc: cheerio.Root, rootUrl: string, imagesPropertyType?: string) {
   let images: string[] = [];
-  let nodes;
-  let src;
+  let nodes: cheerio.Cheerio | null;
+  let src: string | undefined;
   let dic: Record<string, boolean> = {};
 
   const imagePropertyType = imagesPropertyType ?? `og`;
   nodes = metaTag(doc, `${imagePropertyType}:image`, `property`) || metaTag(doc, `${imagePropertyType}:image`, `name`);
 
   if (nodes) {
-    nodes.each((_: number, node: any) => {
-      src = node.attribs.content;
-      if (src) {
-        src = urlObj.resolve(rootUrl, src);
-        images.push(src);
+    nodes.each((_: number, node: cheerio.Element) => {
+      if (node.type !== 'text') {
+        src = node.attribs.content;
+        if (src) {
+          src = urlObj.resolve(rootUrl, src);
+          images.push(src);
+        }
       }
     });
   }
@@ -82,8 +84,10 @@ function getImages(doc: cheerio.Root, rootUrl: string, imagesPropertyType?: stri
       if (nodes?.length) {
         dic = {};
         images = [];
-        nodes.each((_: number, node: any) => {
-          src = node.attribs.src;
+        nodes.each((_: number, node: cheerio.Element) => {
+          if (node.type !== 'text') {
+            src = node.attribs.src;
+          }
           if (src && !dic[src]) {
             dic[src] = true;
             // width = node.attribs.width;
